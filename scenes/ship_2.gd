fa04e7280172c55a15c59765275cb0afadb2ad4c
lizidelyx <alizza.deli28@gmail.com>
2025-02-21 18:53:00 +0700

    tutorial 2 next level

--- scenes/ship_2.gd
+++ scenes/ship_2.gd
@@ -8,11 +8,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("move_up"):
-		move_local_y(-speed * delta, false)
-	
-	if Input.is_action_pressed("move_down"):
-		move_local_y(speed * delta, false)
 
 	if Input.is_action_pressed("move_left"):
 		move_local_x(-speed * delta, false)
