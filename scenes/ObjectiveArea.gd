e1695f4a07f47a30c327ef207ff3b7cd542bf101
lizidelyx <alizza.deli28@gmail.com>
2025-02-21 14:05:18 +0700

    next level of tutorial 2

--- scenes/ObjectiveArea.gd
+++ scenes/ObjectiveArea.gd
@@ -1,5 +1,14 @@
 extends Area2D
 
-func _on_ObjectiveArea_body_entered(body: RigidBody2D):
-	if (body.name == "BlueShip"):
+@export var done: TextureRect
+var LEVEL2 = load("res://scenes/LEVEL2.tscn")
+var MAIN_LEVEL = load("res://scenes/MainLevel.tscn")
+
+func _on_ObjectiveArea_body_entered(body: RigidBody2D) -> void:
+	if body.name == "BlueShip":
 		print("Reached objective!")
+		get_tree().change_scene_to_packed(LEVEL2)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Fall!")
+	get_tree().reload_current_scene()
